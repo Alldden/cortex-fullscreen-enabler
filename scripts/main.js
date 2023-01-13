8eab8eeee85ea7db3628e16d0992a88961050033
Alden <aldenhasanovic@gmail.com>
2023-01-13 18:35:19 +0100

    Bug fixes and improvments 1.2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,32 @@
-let stateCheck = setInterval(() => {
-    if (document.readyState === "complete") {
-        clearInterval(stateCheck);
-        $("iframe.iframe").attr("allow", "fullscreen");
-        $('iframe.iframe').attr('src', $('iframe.iframe').attr('src'));
+fullscreen = ()=> {
+    $("iframe.iframe").attr("allow", "fullscreen");
+    $('iframe.iframe').attr('src', $('iframe.iframe').attr('src'));
+}
+
+var target = document.body;
+
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes) {
+      for (var i = 0; i < mutation.addedNodes.length; i++) {
+        var node = mutation.addedNodes[i];
+        if (node.tagName === 'iframe') {
+          console.log('Iframe detected');
+          observer.disconnect();
+        }
+      }
     }
-}, 150);
+  });
+});
+
+var config = { childList: true, subtree: true };
+
+observer.observe(target, config);
+
+var iframes = document.getElementsByTagName("iframe");
+for(var i = 0; i < iframes.length; i++) {
+  if (iframes[i].classList.contains("iframe")) {
+    fullscreen();
+    observer.disconnect();
+  }
+}
