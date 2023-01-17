12a2446a06c300f34603d564acb14de17192631a
Alden <aldenhasanovic@gmail.com>
2023-01-17 18:39:02 +0100

    Code optimisation & bug fixes and improvments

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,14 @@
-fullscreen = ()=> {
-    $("iframe.iframe").attr("allow", "fullscreen");
-    $('iframe.iframe').attr('src', $('iframe.iframe').attr('src'));
-}
-
-var target = document.body;
-
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
-    if (mutation.addedNodes) {
-      for (var i = 0; i < mutation.addedNodes.length; i++) {
-        var node = mutation.addedNodes[i];
-        if (node.tagName === 'iframe') {
-          console.log('Iframe detected');
-          observer.disconnect();
+    if (mutation.addedNodes.length > 0) {
+      $(mutation.addedNodes).each(function() {
+        if (this.tagName == "IFRAME" && $(this).hasClass("iframe")) {
+          this.setAttribute("allow", "fullscreen");
+          this.contentWindow.location.reload();
         }
-      }
+      });
     }
   });
 });
 
-var config = { childList: true, subtree: true };
-
-observer.observe(target, config);
-
-var iframes = document.getElementsByTagName("iframe");
-for(var i = 0; i < iframes.length; i++) {
-  if (iframes[i].classList.contains("iframe")) {
-    fullscreen();
-    observer.disconnect();
-  }
-}
+observer.observe(document, { childList: true, subtree: true });
